814a193ee46c8c77f4d6f003b4e4a9dd46eb7e25
zahur <zahurmeerun@hotmail.com>
2023-06-07 08:51:26 +0100

    add: react context to manage state

--- state-managment/App.js
+++ state-managment/App.js
@@ -1,11 +1,48 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, Pressable } from 'react-native';
+import { useState, createContext, useContext } from 'react';
 
-export default function App() {
+const loginContext = createContext({});
+
+function Home() {
+
+  const { setLogin } = useContext(loginContext);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Home Page</Text>
       <StatusBar style="auto" />
+      <Pressable>
+        <Button color="#ff5c5c" title='click me' onPress={() => setLogin(true)}></Button>
+      </Pressable>
+    </View>
+  )
+}
+
+function Login() {
+
+  const { setLogin } = useContext(loginContext);
+
+  return (
+    <View style={styles.container}>
+      <Text>Login Page</Text>
+      <StatusBar style="auto" />
+      <Pressable>
+        <Button color="#ff5c5c" title='click me' onPress={() => setLogin(false)}></Button>
+      </Pressable>
+    </View>
+  )
+}
+
+export default function App() {
+
+  const [login, setLogin] = useState(false)
+
+  return (
+    <View style={styles.container}>
+      <loginContext.Provider value={{ login, setLogin }}>
+        {login ? <Login /> : <Home />}
+      </loginContext.Provider>
     </View>
   );
 }
